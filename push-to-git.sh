9de650a3554a6200442c00e0bab0c28635614945
EC2 Default User <ec2-user@ip-172-31-43-176.ec2.internal>
2024-09-18 10:17:25 +0000

    automated script to push code

---
+++ push-to-git.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set Git user name and email for commits (specific to this repository)
+git config user.name "Pavan-D4"
+git config user.email "pavand@gmail.com"
+
+# Navigate to your project directory
+cd /home/ec2-user/work/shell-scripting/
+
+# Add all changed files to Git
+git add .
+
+# Commit the changes with a message containing the current date and time
+git commit -m "Auto-commit: $(date)"
+
+# Push the changes to the remote repo
+git push https://$1@github.com/Pavan-D4/calculator-app.git main 
+
+echo "Changes pushed to Git repository!"
